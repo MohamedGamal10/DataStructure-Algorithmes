ffbb605ebe792c7ab01c49088d7184c762a7f2ae
MohamedGamal10 <50083096+MohamedGamal10@users.noreply.github.com>
2023-05-01 15:30:59 +0200

    commit

--- DataStructure/KeyValuePair-Dictionary/src/Dictionary.java
+++ DataStructure/KeyValuePair-Dictionary/src/Dictionary.java
@@ -1,5 +1,85 @@
-public class Dictionary
+public class Dictionary<Tkey,Tvalue>
 {
-    
+    KeyValuePair[] entries;
+    int initialSize;
+    int entriesCount;
+    public Dictionary ()
+    {
+        this.initialSize = 3;
+        this.entries = new KeyValuePair[this.initialSize];
+        this.entriesCount= 0;
+    }
+    public void ResizeOrNot()
+    {
+        if(this.entriesCount < this.entries.length -1) return;
+
+        int newSize = this.entries.length + this.initialSize;
+        System.out.println("[resize] from " + this.entries.length + " to " + newSize);
+        KeyValuePair[] newArray = new KeyValuePair[newSize];
+        System.arraycopy(this.entries,0, newArray,0,this.entries.length);
+        this.entries = newArray;
+    }
+    public int Size()
+    {
+        return this.entriesCount;
+    }
+    public void Set (Tkey key, Tvalue value)
+    {
+        for (int i = 0; i < this.entries.length; i++)
+        {
+            if(this.entries[i] !=null && this.entries[i].get_key() == key)
+            {
+                this.entries[i].set_value(value);
+                return;
+            }
+        }
+        this.ResizeOrNot();
+        KeyValuePair<Tkey,Tvalue> newPair = new KeyValuePair<>(key,value);
+        this.entries[this.entriesCount] = newPair;
+        this.entriesCount ++;
+    }
+    public Tvalue Get(Tkey key)
+    {
+        for (int i = 0; i < this.entries.length; i++)
+        {
+            if(this.entries[i] !=null && this.entries[i].get_key() == key)
+            {
+                return (Tvalue) this.entries[i].get_value();
+            }
+        }
+        return null;
+    }
+    public boolean Remove (Tkey key)
+    {
+        for (int i = 0; i < this.entries.length; i++)
+        {
+            if(this.entries[i] !=null && this.entries[i].get_key() == key)
+            {
+                this.entries[i] = this.entries[this.entriesCount-1];
+                this.entries[this.entriesCount-1]=null;
+                this.entriesCount --;
+                return true;
+            }
+        }
+        return false;
+    }
+    public void Print()
+    {
+        System.out.println("----------------------------");
+        System.out.println("[size] " + this.Size());
+        for (int i = 0; i < this.entries.length; i++)
+        {
+            if(this.entries[i] ==null)
+            {
+                System.out.println("["+i+"]");
+                continue;
+            }
+            else
+            {
+                System.out.println("[" +i+ "]" + this.entries[i].get_key() + " : " + this.entries[i].get_value());
+            }
+        }
+        System.out.println("----------------------------");
+    }
 
 }
--- DataStructure/KeyValuePair-Dictionary/src/Main.java
+++ DataStructure/KeyValuePair-Dictionary/src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args) {
+        Dictionary<String,String> dic = new Dictionary<>();
+        dic.Print();
+
+        dic.Set("Mohamed","mohamed@gmail.com");
+        dic.Set("ahmed","ahmed@gmail.com");
+        dic.Print();
+
+        dic.Set("gamal","gamal@gmail.com");
+        dic.Set("greti","greti@gmail.com");
+        dic.Set("Artist","Artist@gmail.com");
+
+        //dic.Set("rArtist","rArtist@gmail.com");
+        //dic.Set("tArtist","tArtist@gmail.com");
+        //dic.Set("yArtist","yArtist@gmail.com");
+
+        dic.Print();
+
+        System.out.println(dic.Get("Mohamed"));
+        System.out.println(dic.Get("ahmed"));
+        System.out.println(dic.Get("gamalf"));
+
+        dic.Remove("Mohamed");
+        dic.Remove("ahmed");
+        dic.Remove("gamal");
+        dic.Remove("greti");
+        dic.Remove("Artist");
+        dic.Print();
+        dic.Set("Sinar","Sinar@gmail.com");
+        dic.Print();
 
     }
 }
