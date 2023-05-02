3a29104d87f2d9a4ce15db291207f977cfe2b34e
MohamedGamal10 <50083096+MohamedGamal10@users.noreply.github.com>
2023-05-02 12:29:15 +0200

    commit

---
+++ DataStructure/Hashing_FNV-1a Implementation/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Hash hash = new Hash();
+        hash.Hash32("This is Original Text");
+        hash.Hash64("This is Original Text");
+    }
+}
